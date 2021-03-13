759f729f2f0dac988aa82b1c6116536baf67d58a
Rafael George <george.rafael@gmail.com>
2021-03-13 10:58:49 -0700

    Refactor to separate logic for line and tax processing

--- spec/subscribe_spec.rb
+++ spec/subscribe_spec.rb
@@ -3,20 +3,39 @@ module Subscribe
 
   InvalidBasket = Class.new(StandardError)
   EXEMPT_PRODUCTS = ["book", "chocolate bar"]
-  LINE_PATTERN = /(^\d+)\s(\w+)\sat\s(\d+$)/
+  SALE_TAX = 0.10
+
+  Line = Struct.new(:quantity, :product, :price, keyword_init: true) do
+    LINE_PATTERN = /^(\d+)\s(.+)\sat\s(\d+\.\d+)$/
+    def self.for(line)
+      quantity, product, price = line.scan(LINE_PATTERN).first
+      new(quantity: quantity.to_i, product: product, price: price.to_f)
+    end
+
+    def total
+      price * quantity
+    end
+  end
+
+  SaleTaxes = Struct.new(:tax, :total, keyword_init: true) do
+    def self.for(line)
+      tax = line.price * SALE_TAX unless EXEMPT_PRODUCTS.include? line.product
+
+      new(tax: ("%.2f" % [tax.to_f]).to_f, total: ("%.2f" % [line.total + tax.to_f]).to_f)
+    end
+  end
+
   def call(line)
     raise InvalidBasket if line.to_s.empty?
 
-    sale_taxes = 0.0
-    quantity, product, price = line.scan(LINE_PATTERN).first
+    line = Line.for(line)
 
-    price = price * 0.10 unless EXEMPT_PRODUCTS.include? product
-    sale_taxes += price
+    sale_taxes = SaleTaxes.for(line)
 
     {
-      products: [quantity: quantity, product: product, price: price],
-      sale_taxes: sale_taxes,
-      total: price
+      products: [quantity: line.quantity, product: line.product, price: sale_taxes.total],
+      sale_taxes: sale_taxes.tax,
+      total: sale_taxes.total
     }
   end
 
@@ -41,7 +60,7 @@ RSpec.describe Subscribe do
         {
           "2 book at 12.49" => {
             products: [{product: "book", quantity: 2, price: 24.98}],
-            sale_taxes: 0.0,
+            sale_taxes: 0.00,
             total: 24.98
           },
           "1 music CD at 14.99" => {
@@ -49,9 +68,9 @@ RSpec.describe Subscribe do
             sale_taxes: 1.50,
             total: 16.49
           },
-          "1 chocolate bar at 14.99" => {
+          "1 chocolate bar at 0.85" => {
             products: [{product: "chocolate bar", quantity: 1, price: 0.85}],
-            sale_taxes: 0.0,
+            sale_taxes: 0.00,
             total: 0.85
           },
         }
