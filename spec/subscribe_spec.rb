1f5a6b8f5e9c40b90a95c180de93c12dc32fee6f
Rafael George <george.rafael@gmail.com>
2021-03-15 09:06:46 -0600

    Fix rounding precision logic for sale tax calculations

--- spec/subscribe_spec.rb
+++ spec/subscribe_spec.rb
@@ -2,11 +2,13 @@ module Subscribe
   extend self
 
   InvalidBasket = Class.new(StandardError)
-  EXEMPT_PRODUCTS = ["book", "chocolate bar"]
+  EXEMPT_PRODUCTS = ["book", "chocolate bar", "packet of headache pills"]
   SALE_TAX = 0.10
 
   Line = Struct.new(:quantity, :product, :price, keyword_init: true) do
     LINE_PATTERN = /^(\d+)\s(.+)\sat\s(\d+\.\d+)$/
+    EXEMPT_PRODUCTS = ["book", "chocolate bar", "packet of headache pills"]
+
     def self.for(line)
       quantity, product, price = line.scan(LINE_PATTERN).first
       raise ArgumentError if quantity.to_s.empty? || price.to_s.empty?  || product.to_s.empty?
@@ -18,6 +20,10 @@ module Subscribe
       price * quantity
     end
 
+    def exempt?
+      EXEMPT_PRODUCTS.include? product
+    end
+
     def to_h
       {
         product: product,
@@ -27,25 +33,53 @@ module Subscribe
     end
   end
 
-  SaleTaxes = Struct.new(:tax, :total, keyword_init: true) do
+  SaleTaxes = Struct.new(:line, :tax, keyword_init: true) do
     def self.for(line)
-      tax = line.price * SALE_TAX unless EXEMPT_PRODUCTS.include? line.product
+      tax = SALE_TAX unless line.exempt?
 
-      new(tax: ("%.2f" % [tax.to_f]).to_f, total: ("%.2f" % [line.total + tax.to_f]).to_f)
+      new(line: line, tax: ("%.2f" % [tax.to_f]).to_f)
+    end
+
+    def to_h
+      {
+        product: product,
+        total_price: total_price,
+        quantity: quantity,
+        taxes: taxes,
+      }
+    end
+
+    def total_price
+      format("%.2f", taxes + line.total).to_f
+    end
+
+    def quantity
+      line.quantity
+    end
+
+    def product
+      line.product
+    end
+
+    def price
+      line.price
+    end
+
+    def taxes
+      format("%.2f", ((line.total * tax) / 0.05).round * 0.05).to_f
     end
   end
 
   def call(line)
     raise InvalidBasket if line.to_s.empty?
-
     line = Line.for(line)
 
-    sale_taxes = SaleTaxes.for(line)
+    line_sale_taxes = SaleTaxes.for(line)
 
     {
-      products: [quantity: line.quantity, product: line.product, price: sale_taxes.total],
-      sale_taxes: sale_taxes.tax,
-      total: sale_taxes.total
+      products: [line_sale_taxes.to_h],
+      sale_taxes: line_sale_taxes.taxes,
+      total: line_sale_taxes.total_price
     }
   end
 
@@ -69,20 +103,30 @@ RSpec.describe Subscribe do
       let(:baskets)do
         {
           "2 book at 12.49" => {
-            products: [{product: "book", quantity: 2, price: 24.98}],
+            products: [{product: "book", quantity: 2, total_price: 24.98, taxes: 0.0}],
             sale_taxes: 0.00,
             total: 24.98
           },
           "1 music CD at 14.99" => {
-            products: [{product: "music CD", quantity: 1, price: 16.49}],
+            products: [{product: "music CD", quantity: 1, total_price: 16.49, taxes: 1.5}],
             sale_taxes: 1.50,
             total: 16.49
           },
           "1 chocolate bar at 0.85" => {
-            products: [{product: "chocolate bar", quantity: 1, price: 0.85}],
+            products: [{product: "chocolate bar", quantity: 1, total_price: 0.85, taxes: 0.0}],
             sale_taxes: 0.00,
             total: 0.85
           },
+          "1 packet of headache pills at 9.75" => {
+            products: [{product: "packet of headache pills", quantity: 1, total_price: 9.75, taxes: 0.0}],
+            sale_taxes: 0.00,
+            total: 9.75
+          },
+          "1 bottle of perfume at 18.99" => {
+            products: [{product: "bottle of perfume", quantity: 1, total_price: 20.89, taxes: 1.9}],
+            sale_taxes: 1.90,
+            total: 20.89
+          }
         }
       end
 
@@ -120,5 +164,87 @@ RSpec.describe Subscribe do
         end
       end
     end
+
+    describe "#exempt?" do
+      context 'when the product is book, medicine or food' do
+        let(:lines) do
+          [
+            "1 book at 10.00",
+            "1 chocolate bar at 5.00",
+            "1 packet of headache pills at 3.00"
+          ]
+        end
+
+        it 'returns `true` for exempt product' do
+          lines.each do |attributes|
+            expect(line.for(attributes)).to be_exempt
+          end
+        end
+      end
+
+      context 'when the product is not a book, medicine or food' do
+        let(:lines) do
+          [
+            "1 music CD at 10.00",
+            "1 bottle of perfume at 20.00",
+          ]
+        end
+
+        it 'returns `false` for exempt product' do
+          lines.each do |attributes|
+            expect(line.for(attributes)).not_to be_exempt
+          end
+        end
+      end
+
+    end
   end
+
+  describe Subscribe::SaleTaxes do
+    subject(:sale_taxes) { described_class }
+
+    describe '.for' do
+      context 'when passing general goods' do
+        let(:lines) do
+          {
+            Subscribe::Line.new(quantity: 1, product: "music CD", price: 14.99) => {
+              quantity: 1, product: "music CD", total_price: 16.49, taxes: 1.50
+            },
+            Subscribe::Line.new(quantity: 1, product: "bottle of perfume", price: 18.99) => {
+              quantity: 1, product: "bottle of perfume", total_price: 20.89, taxes: 1.90
+            }
+          }
+        end
+
+        it 'apply 10% tax' do
+          lines.each_pair do |line, expectation|
+            expect(sale_taxes.for(line).to_h).to match(expectation)
+          end
+        end
+      end
+    end
+
+    context 'when passing food, books or medicine' do
+      let(:lines) do
+        {
+          Subscribe::Line.new(quantity: 1, product: "book", price: 12.49) => {
+            quantity: 1, product: "book", total_price: 12.49, taxes: 0.0
+          },
+          Subscribe::Line.new(quantity: 1, product: "chocolate bar", price: 0.85) => {
+            quantity: 1, product: "chocolate bar", total_price: 0.85, taxes: 0.0,
+          },
+          Subscribe::Line.new(quantity: 1, product: "packet of headache pills", price: 9.75) => {
+            quantity: 1, product: "packet of headache pills", total_price: 9.75, taxes: 0.0
+          },
+        }
+      end
+
+      it 'does not apply 10% tax' do
+        lines.each_pair do |line, expectation|
+          expect(sale_taxes.for(line).to_h).to match(expectation)
+        end
+      end
+    end
+  end
+
 end
